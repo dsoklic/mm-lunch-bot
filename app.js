291c9a65c369b372179e874905edd8162b0df386
Domen Soklic <domen.soklic@cosylab.com>
2019-06-10 19:52:01 +0200

    Changed code so Heroku can set the port.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const $ = require('cheerio');
 var TurndownService = require('turndown')
 var turndownService = new TurndownService()
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const viaBonaUrl = 'https://www.via-bona.com/sl/ponudba-hrane/malice-in-kosila/'
 
