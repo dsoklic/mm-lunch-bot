9a892b2d332a6024a4a96990453242db6c272cac
Domen Soklic <domen.soklic@cosylab.com>
2019-06-10 19:49:15 +0200

    Added support for Via Bona

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const rp = require('request-promise')
+const $ = require('cheerio');
+var TurndownService = require('turndown')
+var turndownService = new TurndownService()
+const app = express()
+const port = 3000
+
+const viaBonaUrl = 'https://www.via-bona.com/sl/ponudba-hrane/malice-in-kosila/'
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.get('/', (req, res) => getLunchInfo(res))
+
+function getLunchInfo(res) {
+    rp(viaBonaUrl).then((html) => {
+        //success!
+        let viaBonaMenu = turndownService.turndown($('div.ck-text', html).html())
+        let menu = "#Via Bona\n" + viaBonaMenu
+
+        res.json({'text': menu})
+    }).catch((err) => {
+        // handle error
+        console.error(err)
+        res.json({'text': err.message})
+    });
+}
