24f236cfec9c616fc502af63584771914638a8fd
Domen Soklic <domen.soklic@cosylab.com>
2019-06-11 17:32:47 +0200

    Added support for Kurji tat

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const turndownService = new TurndownService();
 const app = express();
 const port = process.env.PORT || 8000
 
-const viaBonaUrl = 'https://www.via-bona.com/sl/ponudba-hrane/malice-in-kosila/';
-
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 app.get('/', (req, res) => getLunchInfo(res));
 app.post('/', (req, res) => getLunchInfo(res));
@@ -20,7 +18,8 @@ app.post('/', (req, res) => getLunchInfo(res));
 function getLunchInfo(res) {
     // Execute all menu requests in parallel.
     async.parallel([
-        getViaBonaMenu
+        getViaBonaMenu,
+        getKurjiTatMenu
     ], (err, results) => {
         if (err != null) {
             // handle error
@@ -35,14 +34,32 @@ function getLunchInfo(res) {
 }
 
 /**
- * Get the menu from Via Bona
+ * Get the menu from Via Bona.
  * @param callback Callback that is called with the result.
  */
 function getViaBonaMenu(callback) {
+    let viaBonaUrl = 'https://www.via-bona.com/sl/ponudba-hrane/malice-in-kosila/';
+
     rp(viaBonaUrl).then((html) => {
         //success!
         let viaBonaMenu = turndownService.turndown($('div.ck-text', html).html());
         let menu = "#Via Bona\n" + viaBonaMenu;
+
+        callback(null, menu);
+    }).catch(callback);
+}
+
+/**
+ * Get the menu from Kurji Tat.
+ * @param callback Callback that is called with the result.
+ */
+function getKurjiTatMenu(callback) {
+    let kujiTatUrl = "https://docs.google.com/document/u/1/d/e/2PACX-1vShhBZHuTFuFZxZIS2fnWCZrMuKGHpVtYsWjuik02i_S7CMAYo8zRjS5p3tm9WqsEY3KqssFkHZY-kI/pub?embedded=true";
+
+    rp(kujiTatUrl).then((html) => {
+        //success!
+        let kurjiTatMenu = turndownService.turndown($('table', html).html());
+        let menu = "#Kurji tat\n" + kurjiTatMenu;
 
         callback(null, menu);
     }).catch(callback);
